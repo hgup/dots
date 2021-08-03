73eb4aa3da1be81aec6ef8516f23bb2e95f9c630
Hursh Gupta <hurshgupta1@gmail.com>
2021-08-03 21:51:57 +0530

    install vim-plug automatically

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -20,3 +20,7 @@ scoop install $apps
 
 # Run Update Automatically
 ./update.ps1
+
+# for NVIM
+iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |` ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
+
